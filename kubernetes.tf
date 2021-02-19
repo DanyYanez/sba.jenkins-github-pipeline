706535cc02316d8b37f7fcab4c6a043796694e07
DanyYanez <dany2332@hotmail.es>
2021-02-19 10:55:59 -0500

    terraform

---
+++ kubernetes.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "docker" {
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "danyyanez/sba_kuber"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
+
+
+
+
+
+#resource "docker_image" "flask" {
+#  name         = "habboubi/flaskapp:latest"
+#  keep_locally = false
+#}
+
+#resource "docker_container" "flask" {
+#  image = docker_image.flask.latest
+#  name  = "flaskapp"
+#  ports {
+#    internal = 80
+#    external = 80
+#  }
+#}
+
